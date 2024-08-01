6e0d9bc26dc7c80f80ad6cd66ab64cf475c584ae
Orbitadoo <juanpaavaldezz@gmail.com>
2024-08-01 19:51:27 -0300

    [EDITED] AboutUs Page

---
+++ src/app/(pages)/(public)/aboutus/page.jsx
@@ -0,0 +1,29 @@
+import UsersCard from "@/app/components/UsersCard";
+import teamMembers from "@/app/utils/team-members.json";
+
+const AboutUs = () => {
+  return (
+    <section id="aboutus">
+      <h2 className="text-3xl font-bold text-primary">Our Team</h2>
+      <p>Our team is made up of experienced developers and designers.</p>
+
+      <div className="flex justify-center mt-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
+          {teamMembers.map((member) => (
+            <UsersCard
+              key={member.name}
+              picture={member.image}
+              name={member.name}
+              lastname={member.lastname}
+              position={member.position}
+              github={member.github}
+              linkedin={member.linkedin}
+            />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutUs;
--- src/app/components/UsersCard.jsx
+++ src/app/components/UsersCard.jsx
@@ -1,20 +1,46 @@
 import Image from "next/image";
-import teamMembers from "@/app/utils/team-members.json";
-const UsersCard = ({ picture }) => {
+import Link from "next/link";
+const UsersCard = ({ picture, lastname, name, position, github, linkedin }) => {
   return (
-    <div className="bg-white max-w-fit pb-4">
+    <div id="usersCard" className="bg-white max-w-fit shadow-lg rounded-lg">
       <Image
         src={picture}
-        alt="Picture of the User"
+        alt="A picture of the member team"
         width={500}
         height={500}
         className="size-72"
       />
 
-      <h3 className="text-xl">
-        Leonardo <span className="font-bold">Valdez</span>
-      </h3>
-      <p className="text-gray-600">Frontend Developer</p>
+      <div className="py-2 px-4 text-center">
+        <h3 className="text-xl">
+          {lastname}, <span className="font-bold text-primary">{name}</span>
+        </h3>
+        <p className="text-gray-600">{position}</p>
+      </div>
+
+      <div className="flex text-center">
+        <Link
+          href={github}
+          target="_blank"
+          rel="noreferrer"
+          className="flex justify-center text-white font-bold px-4 py-2 bg-primary hover:opacity-90 w-full ease-in-out duration-300"
+        >
+          <Image src="/github.svg" alt="github logo" width={20} height={20} />
+        </Link>
+        <Link
+          href={linkedin}
+          target="_blank"
+          rel="noreferrer"
+          className="flex justify-center text-white font-bold px-4 py-2 bg-primary hover:opacity-90 w-full ease-in-out duration-300"
+        >
+          <Image
+            src="/linkedin.svg"
+            alt="linkedin logo"
+            width={20}
+            height={20}
+          />
+        </Link>
+      </div>
     </div>
   );
 };
