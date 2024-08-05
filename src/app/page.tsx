35a38929d2fe86952d7030ee09c7054839f9a886
Carmelo C <carmelo2055@gmail.com>
2024-08-05 12:02:07 -0300

    landing page basic styles and functions added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,21 @@
 import Heading from "@/components/Writing/Heading";
 import SubTitle from "@/components/Writing/SubTitle";
 import Paragraph from "@/components/Writing/Paragraph";
-import Link from "next/link"
+import Link from "next/link";
 
 const App = () => {
   return (
-    <main >
-      <div className="pt-64 text-center">
+    <main className="flex flex-col items-center pt-32 space-y-16 min-h-screen bg-gray-50">
+      <div className="text-center">
         <Heading text="Welcome to GlobCamp" />
       </div>
-      <div className="flex flex-row space-x-32 items-start min-h-screen">
-      <div className="pt-20 ml-64 size-64 ">
-        <Paragraph text="Discover the essential knowledge every JavaScript developer should know. Progress through our expertly crafted topics and challenge yourself with quizzes at the end of each lesson. Earn knowledge points for every correct answer and track your progress as you master JavaScript. Ready to begin? " />
+      <div className="max-w-2xl text-center px-4">
+        <Paragraph text="Discover the essential knowledge every JavaScript developer should know. Progress through our expertly crafted topics and challenge yourself with quizzes at the end of each lesson. Earn knowledge points for every correct answer and track your progress as you master JavaScript. Ready to begin?" />
       </div>
-      <div className="pt-20 size-64">
-        <Link href="" className="pt-64 text-center" >
+      <div className="max-w-lg text-center px-4">
+        <Link href="" className="text-blue-500 hover:text-blue-700 transition-colors">
             <SubTitle text="Click here to start your learning journey!" />
         </Link>
-      </div>
       </div>
     </main>
   );
